0db7cecb5e9b099d32d4c25c6bb458ecf010f025
roberto <roberto.sodini1@gmail.com>
2025-07-13 13:26:31 +0200

    Configuration service e Gateway Service

---
+++ gateway-service/src/main/java/com/app_fitness/getaway_service/GetawayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app_fitness.getaway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetawayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetawayServiceApplication.class, args);
+	}
+
+}
